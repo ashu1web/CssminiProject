fcd60522689114dc459429895498d7cefef3ac81
ashu1web <113449680+ashu1web@users.noreply.github.com>
2024-08-19 14:20:49 +0530

    Add files via upload

---
+++ Tailwind/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      './dist/**/*.html', // Correct path for HTML files in the dist directory
+    ],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
